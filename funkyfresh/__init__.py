23f56196ca1bcb04e0934a07d2a70534926caa45
Zachariah Milby <zmilby@caltech.edu>
2023-03-23 15:30:19 -0700

    Forgot to add new function to available imports

--- funkyfresh/__init__.py
+++ funkyfresh/__init__.py
@@ -1,2 +1,3 @@
 from funkyfresh.core import set_aas_style, set_agu_style, \
-    set_personal_whitepaper_style, set_caltech_thesis_style
+    set_personal_whitepaper_style, set_caltech_thesis_style, \
+    set_latex_default_style
